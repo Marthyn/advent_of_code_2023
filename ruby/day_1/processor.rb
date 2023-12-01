f84f0ba4b6fed02776f43d1f3ef675ad33b139f4
Marthyn Olthof <marthyn.olthof@beequip.nl>
2023-12-01 20:35:58 +0100

    Solve day 1, star 2

--- ruby/day_1/processor.rb
+++ ruby/day_1/processor.rb
@@ -1,11 +1,40 @@
 require 'pry'
 
+NUMBERS = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9'
+}.freeze
+
 def calculate(input)
   input.lines.map do |line|
-    numbers = line.delete('^0-9')
+    numbers = find_numbers(line)
 
-    first, last = [numbers[0], numbers[-1]]
-    "#{first}#{last}".to_i
+    [numbers[0], numbers[-1]].join.to_i
   end
+end
+
+def find_numbers(line)
+  max_value = line.length - 1
+  numbers = []
+  start = 0
+  while start < max_value
+    (start..max_value).each do |take|
+      word = line[start..take]
+      if NUMBERS.keys.include?(word.to_sym)
+        numbers << NUMBERS[word.to_sym]
+      elsif number = NUMBERS.values.find { |n| n == word }
+        numbers << number
+      end
+    end
+    start += 1
+  end
+  numbers
 end
 
--- ruby/day_1/test.rb
+++ ruby/day_1/test.rb
@@ -1,15 +1,16 @@
 require '../day_1/processor'
 
-file = File.open('verify_input.txt')
+file = File.open('verify_input_2.txt')
 
 input = file.read
 
-puts calculate(input)
-puts calculate(input).sum == 142 # Success
+calculate(input)
+puts calculate(input).sum == 281 # Success
+
 
 file = File.open('input.txt')
 
 input = file.read
 
-puts calculate(input)
-puts calculate(input).sum == 55538 # Success
+calculate(input)
+puts calculate(input).sum == 54875 # Success
